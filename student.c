2d3bc5bec7740e077fbaf391845115e0179c1d60
darshan2816 <obito28160@gmail.com>
2025-05-13 15:44:36 +0530

    student.c

---
+++ student.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int c;
+int shridevi()
+{
+    printf("darshan\n");
+    printf("date of birth \n");
+    printf("age\n");
+    printf("mobile number\n");
+}
+int main()
+{
+    shridevi();
+    return 0;
+}
